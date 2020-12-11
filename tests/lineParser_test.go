aa5ab85b142f7c9f418e083cb4042d7601b5f9bc
Cameron Perera <cameron.perera@samaritanministries.org>
2020-12-11 16:21:16 -0600

    Add Unit tests

---
+++ tests/lineParser_test.go
@@ -0,0 +1,19 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/cameronperera/quiz/src/helpers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseLines(t *testing.T) {
+	var expected = []helpers.Problem{
+		{Question: "1+1", Answer: "2"},
+		{Question: "1+2", Answer: "3"},
+	}
+	lines := [][]string{{"1+1", "2"}, {"1+2", "3"}}
+	var actual = helpers.ParseLines(lines)
+
+	assert.Equal(t, expected, actual)
+}
